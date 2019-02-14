9ad3dcad28c81bf49ce01d8ed1fe611fa0e2b95a
Cary Miller <cary.a.miller@gmail.com>
2019-02-14 00:29:35 -0800

    Add REST class

---
+++ The-10-Cary/The-10-Cary/REST.swift
@@ -0,0 +1,55 @@
+//
+//  REST.swift
+//  The-10-Cary
+//
+//  Created by Cary Miller on 2/13/19.
+//  Copyright © 2019 C.Miller & Co. All rights reserved.
+//
+
+import Foundation
+
+class REST {
+
+   public static func request(path: String, method: String, parameters: [String:String]?, headers: [String:String]?, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+      guard var components = URLComponents(string: path) else {
+         print("Error: could not create URL components")
+         return
+      }
+
+      // GET: parameters = Query string parameters
+      if method == "GET", let parameters = parameters {
+         components.queryItems = parameters.map { (key, value) in
+            URLQueryItem(name: key, value: value)
+         }
+         components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+      }
+
+      var request = URLRequest(url: components.url!)
+
+      // POST or PUT: parameters = Request body parameters
+      if method == "POST" || method == "PUT" {
+         if let parameters = parameters {
+            do {
+               request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            } catch let error {
+               print(error)
+            }
+         }
+      }
+
+      request.httpMethod = method
+
+      if let headers = headers {
+         for (key, value) in headers {
+            request.setValue(value, forHTTPHeaderField: key)
+         }
+      }
+
+      let session = URLSession.shared
+      let task = session.dataTask(with: request) {
+         (data, response, error) in
+         completion(data, response, error)
+      }
+      task.resume()
+   }
+}
