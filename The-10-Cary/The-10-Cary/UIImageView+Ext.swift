d19064b47b62cefa1dee589edf59547f48d81622
Cary Miller <cary.a.miller@gmail.com>
2019-02-14 00:31:51 -0800

    add extensions for UIImageView

---
+++ The-10-Cary/The-10-Cary/UIImageView+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+Ext.swift
+//  GistView
+//
+//  Created by Cary Miller on 8/21/18.
+//  Copyright © 2018 C.Miller & Co. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImageView {
+   public func transition(toImage image: UIImage?) {
+      UIView.transition(
+         with: self,
+         duration: 0.3,
+         options: [.transitionCrossDissolve],
+         animations: {
+            self.image = image
+         },
+         completion: nil
+      )
+   }
+}
