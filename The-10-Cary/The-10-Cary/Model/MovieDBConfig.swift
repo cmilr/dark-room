9a6dc650b1e7f8bd18c6c11768c88aad8d1eb0d4
Cary Miller <cary.a.miller@gmail.com>
2019-02-14 00:31:04 -0800

    Add MovieDBConfig class and two new groupe

---
+++ The-10-Cary/The-10-Cary/Model/MovieDBConfig.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDBConfig.swift
+//  The-10-Cary
+//
+//  Created by Cary Miller on 2/13/19.
+//  Copyright © 2019 C.Miller & Co. All rights reserved.
+//
+
+import Foundation
+
+struct MovieDBConfig {
+   var baseURL: String?
+   var posterSizes: [String]?
+
+   init() {
+      self.baseURL = ""
+      self.posterSizes = []
+   }
+
+   init?(json: [String: Any]) {
+      guard let baseURL = json["base_url"] as? String,
+         let posterSizes = json["poster_sizes"] as? [String] else {
+            print("Error: could not initialize properties")
+            return nil
+      }
+
+      self.baseURL = baseURL
+      self.posterSizes = posterSizes
+   }
+}
---
+++ The-10-Cary/The-10-Cary/New Group/Movie.swift
@@ -0,0 +1,35 @@
+//
+//  Movie.swift
+//  The-10-Cary
+//
+//  Created by Cary Miller on 2/13/19.
+//  Copyright © 2019 C.Miller & Co. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+   var id: Int?
+   var title: String?
+   var overview: String?
+   var posterPath: String?
+   var backdropPath: String?
+
+   init?(json: [String: Any]) {
+      guard let id = json["id"] as? Int,
+         let title = json["title"] as? String,
+         let overview = json["overview"] as? String else {
+            return nil
+      }
+
+      self.id = id
+      self.title = title
+      self.overview = overview
+
+      if let posterPath = json["poster_path"] as? String,
+         let backdropPath = json["backdrop_path"] as? String {
+         self.posterPath = posterPath
+         self.backdropPath = backdropPath
+      }
+   }
+}
