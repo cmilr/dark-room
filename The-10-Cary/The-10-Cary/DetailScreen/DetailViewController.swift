8a77eb5bb2d30e4172a795cdc5b745496f8dd7d5
Cary Miller <cary.a.miller@gmail.com>
2019-02-14 15:08:07 -0800

    Configure gradientView

---
+++ The-10-Cary/The-10-Cary/DetailScreen/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  The-10-Cary
+//
+//  Created by Cary Miller on 2/14/19.
+//  Copyright © 2019 C.Miller & Co. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+   @IBOutlet weak var gradientView: UIView!
+   @IBOutlet weak var overviewLabel: UILabel!
+   var movie = Movie()
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      if let overview = movie.overview {
+         overviewLabel.text = overview
+      }
+   }
+
+   override func viewDidLayoutSubviews() {
+      configureGradientView()
+   }
+
+   private func configureGradientView() {
+      if let colorOne = UIColor(named: "moviePurple")?.cgColor,
+         let colorTwo = UIColor(named: "movieDarkPurple")?.cgColor {
+         let gradient = CAGradientLayer(start: .topLeft, end: .bottomRight, colors: [colorOne, colorTwo], type: .axial)
+         gradient.frame = gradientView.bounds
+         gradientView.layer.insertSublayer(gradient, at: 0)
+      }
+   }
+}
