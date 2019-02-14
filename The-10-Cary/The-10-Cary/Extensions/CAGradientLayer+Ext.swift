3fcc0c3b860949faa590a3bb8f4d512c82e480b5
Cary Miller <cary.a.miller@gmail.com>
2019-02-14 15:07:34 -0800

    Add CAGradientLayer extension

---
+++ The-10-Cary/The-10-Cary/Extensions/CAGradientLayer+Ext.swift
@@ -0,0 +1,56 @@
+//
+//  CAGradientLayer+Ext.swift
+//  The-10-Cary
+//
+//  Created by Cary Miller on 2/14/19.
+//  Copyright © 2019 C.Miller & Co. All rights reserved.
+//
+
+import UIKit
+
+extension CAGradientLayer {
+
+   enum Point {
+      case topLeft
+      case centerLeft
+      case bottomLeft
+      case topCenter
+      case center
+      case bottomCenter
+      case topRight
+      case centerRight
+      case bottomRight
+
+      var point: CGPoint {
+         switch self {
+         case .topLeft:
+            return CGPoint(x: 0, y: 0)
+         case .centerLeft:
+            return CGPoint(x: 0, y: 0.5)
+         case .bottomLeft:
+            return CGPoint(x: 0, y: 1.0)
+         case .topCenter:
+            return CGPoint(x: 0.5, y: 0)
+         case .center:
+            return CGPoint(x: 0.5, y: 0.5)
+         case .bottomCenter:
+            return CGPoint(x: 0.5, y: 1.0)
+         case .topRight:
+            return CGPoint(x: 1.0, y: 0.0)
+         case .centerRight:
+            return CGPoint(x: 1.0, y: 0.5)
+         case .bottomRight:
+            return CGPoint(x: 1.0, y: 1.0)
+         }
+      }
+   }
+
+   convenience init(start: Point, end: Point, colors: [CGColor], type: CAGradientLayerType) {
+      self.init()
+      self.startPoint = start.point
+      self.endPoint = end.point
+      self.colors = colors
+      self.locations = (0..<colors.count).map(NSNumber.init)
+      self.type = type
+   }
+}
