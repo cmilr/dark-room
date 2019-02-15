7400d2185099218355e52c169432e0d8d337cafc
Cary Miller <cary.a.miller@gmail.com>
2019-02-15 10:58:06 -0800

    Handle title, ratings, and genres

--- The-10-Cary/The-10-Cary/DetailScreen/DetailViewController.swift
+++ The-10-Cary/The-10-Cary/DetailScreen/DetailViewController.swift
@@ -11,19 +11,52 @@ import UIKit
 class DetailViewController: UIViewController {
 
    @IBOutlet weak var gradientView: UIView!
+   @IBOutlet weak var titleLabel: UILabel!
+   @IBOutlet weak var ratingsLabel: UILabel!
+   @IBOutlet weak var genreLabel: UILabel!
    @IBOutlet weak var overviewLabel: UILabel!
+   @IBOutlet weak var posterImageView: UIImageView!
+
    var movie = Movie()
 
    override func viewDidLoad() {
       super.viewDidLoad()
-      configureOverviewLabel()
+      setTitleLabel()
+      setRating()
+      setGenres()
+      setOverviewLabel()
    }
 
    override func viewDidLayoutSubviews() {
       configureGradientView()
    }
 
-   private func configureOverviewLabel() {
+   private func setTitleLabel() {
+      titleLabel.text = movie.title
+   }
+
+   private func setRating() {
+      if let rawAverage = movie.voteAverage {
+         let average = (rawAverage / 2).rounded()
+         let ratingString = String( Array(repeating: "★", count: Int(average)) )
+         ratingsLabel.text = ratingString
+      }
+   }
+
+   private func setGenres() {
+      if let genres = movie.genreIDs {
+         var genreString = ""
+         for genre in genres {
+            if let genreTitle = genreDict[genre] {
+               genreString += "\(genreTitle), "
+            }
+         }
+         genreString = String(genreString.dropLast(2))
+         genreLabel.text = genreString
+      }
+   }
+
+   private func setOverviewLabel() {
       if let overview = movie.overview {
          overviewLabel.text = overview
          overviewLabel.setLineHeight(1.5)
