ab19a34291918e73bd8b6d77be8cebdf5f09f26c
Cary Miller <cary.a.miller@gmail.com>
2019-02-17 17:42:20 -0800

    Handle centering of current cell upon navigating back from detail

--- The-10-Cary/The-10-Cary/MasterScreen/MasterViewController.swift
+++ The-10-Cary/The-10-Cary/MasterScreen/MasterViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class MasterViewController: UIViewController {
 
+   @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
    @IBOutlet weak var collectionView: UICollectionView!
    @IBOutlet weak var segmentedControl: UISegmentedControl!
    @IBOutlet weak var collectionViewYConstraint: NSLayoutConstraint!
    @IBOutlet weak var segmentedControlYConstraint: NSLayoutConstraint!
    var imageCache = [String: UIImage?]()
    var movies = [Movie]()
+   var currentCell = 0
    var nowPlaying = true
    var switchingCategories = false
-   var collectionViewX = CGFloat(0.0)
 
    override func viewDidLoad() {
       super.viewDidLoad()
@@ -29,7 +30,7 @@ class MasterViewController: UIViewController {
    override func viewWillAppear(_ animated: Bool) {
       super.viewWillAppear(animated)
       loadMovies()
-      configureSegmentedControl()
+      centerCurrentCell()
    }
 
    override func viewDidLayoutSubviews() {
@@ -37,6 +38,16 @@ class MasterViewController: UIViewController {
       configureLayout()
       configureGradient()
       configureSegmentedControl()
+   }
+
+   private func centerCurrentCell() {
+      if self.movies.count > 0 {
+         collectionView.scrollToItem(
+            at: IndexPath(row: currentCell, section: 0),
+            at: .centeredHorizontally,
+            animated: false
+         )
+      }
    }
 
    private func configureGradient() {
@@ -59,6 +70,8 @@ class MasterViewController: UIViewController {
    }
 
    private func loadMovies() {
+      activityIndicator.startAnimating()
+      activityIndicator.isHidden = false
       let baseURL = nowPlaying ? BaseURL.nowPlaying : BaseURL.upcoming
       NetworkManager.shared.fetchMovies(from: baseURL) {
          (result) in
@@ -88,21 +101,31 @@ class MasterViewController: UIViewController {
    }
 
    private func configureLayout() {
-      var widthReduction: CGFloat = 40.0
-      var heightReduction: CGFloat = 20.0
+      // Default offset settings
+      var widthOffset: CGFloat = 40.0
+      var heightOffset: CGFloat = 20.0
+      var leftSectionInset: CGFloat = 20.00
+      var rightSectionInset: CGFloat = 20.00
+
+      // If device is iPhone X or taller, zero-out all offsets to allow
+      // contentView cell to center and make use of taller screen.
       if UIScreen.main.bounds.height >= 812 {
-         widthReduction = 0
-         heightReduction = 0
+         widthOffset = 0
+         heightOffset = 0
+         leftSectionInset = 0
+         rightSectionInset = 0
          collectionViewYConstraint.constant = 50
       }
+
       if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
          flowLayout.itemSize = CGSize(
-            width: collectionView.bounds.width - widthReduction,
-            height: collectionView.bounds.height - heightReduction
+            width: collectionView.bounds.width - widthOffset,
+            height: collectionView.bounds.height - heightOffset
          )
          flowLayout.minimumLineSpacing = CGFloat(20.0)
+         flowLayout.sectionInset.left = leftSectionInset
+         flowLayout.sectionInset.right = rightSectionInset
       }
-      collectionViewX = collectionView.contentOffset.x
    }
 
    private func configureSegmentedControl() {
@@ -114,12 +137,14 @@ class MasterViewController: UIViewController {
       }
    }
 
-   private func loadImage(for movie: Movie, into cell: MovieCell) {
+   private func loadImage(for movie: Movie, into cell: MovieCell, at indexPath: IndexPath) {
       guard let urlString = movie.composedPosterPath else {
          return
       }
       if let cachedImage = imageCache[urlString] {
          cell.movieImageView.image = cachedImage
+         self.activityIndicator.stopAnimating()
+         self.activityIndicator.isHidden = true
       } else {
          NetworkManager.shared.imageFrom(urlString) { (image, error) in
             guard error == nil else {
@@ -128,6 +153,8 @@ class MasterViewController: UIViewController {
             }
             self.imageCache[urlString] = image
             DispatchQueue.main.async { [] in
+               self.activityIndicator.stopAnimating()
+               self.activityIndicator.isHidden = true
                cell.movieImageView.transition(toImage: image)
             }
          }
@@ -159,12 +186,11 @@ extension MasterViewController: UICollectionViewDataSource {
 
    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCell
-
       let movie = movies[indexPath.row]
       cell.movieTitleLabel.text = movie.title
-      cell.movieImageView.image = nil
+      cell.movieImageView.image = UIImage(named: "image-placeholder")
       cell.configure()
-      loadImage(for: movie, into: cell)
+      loadImage(for: movie, into: cell, at: indexPath)
 
       return cell
    }
@@ -172,9 +198,15 @@ extension MasterViewController: UICollectionViewDataSource {
 
 extension MasterViewController: UICollectionViewDelegate {
 
+   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+      currentCell = indexPath.row
+   }
+
+   // Custom refresh control for horizontal UICollectionViews.
    func scrollViewDidScroll(_ scrollView: UIScrollView) {
       let reloadDistance = CGFloat(-75.0)
       if collectionView.contentOffset.x < reloadDistance {
+         print("Load movies")
          loadMovies()
       }
    }
