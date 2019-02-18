c79e5b00e66edc9308bc8f97a9b619b4ab2bc1fe
Cary Miller <cary.a.miller@gmail.com>
2019-02-18 00:50:02 -0800

    Add fadeIn and fadeOut animation extension to UIView

---
+++ The-10-Cary/The-10-Cary/Extensions/UIView+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Ext.swift
+//  The-10-Cary
+//
+//  Created by Cary Miller on 2/18/19.
+//  Copyright © 2019 C.Miller & Co. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+   func fadeIn(_ duration: TimeInterval = 1, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+      UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+         self.alpha = 0.8
+      }, completion: completion)  }
+
+   func fadeOut(_ duration: TimeInterval = 1, delay: TimeInterval = 1.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+      UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+         self.alpha = 0
+      }, completion: completion)
+   }
+}
