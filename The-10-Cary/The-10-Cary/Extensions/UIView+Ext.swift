e2bd286b6174f127aba850c3ed418cdeead2306a
Cary Miller <cary.a.miller@gmail.com>
2019-02-18 09:27:25 -0800

    Update alpha

--- The-10-Cary/The-10-Cary/Extensions/UIView+Ext.swift
+++ The-10-Cary/The-10-Cary/Extensions/UIView+Ext.swift
@@ -12,12 +12,12 @@ import UIKit
 extension UIView {
 
    func fadeIn(_ duration: TimeInterval = 1, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
-      UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
-         self.alpha = 0.8
+      UIView.animate(withDuration: duration, delay: delay, options: .curveEaseInOut, animations: {
+         self.alpha = 0.85
       }, completion: completion)  }
 
    func fadeOut(_ duration: TimeInterval = 1, delay: TimeInterval = 1.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
-      UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+      UIView.animate(withDuration: duration, delay: delay, options: .curveEaseInOut, animations: {
          self.alpha = 0
       }, completion: completion)
    }
