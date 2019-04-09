1d6dd1194147b8994928f20e978e910837039a04
Cary Miller <cary.a.miller@gmail.com>
2019-04-09 08:32:12 -0700

    Comment-out debugging info

--- DarkRoom/DarkRoom/Networking/RESTful.swift
+++ DarkRoom/DarkRoom/Networking/RESTful.swift
@@ -9,47 +9,48 @@
 import Foundation
 
 class RESTful {
-
-   public static func request(path: String, method: String, parameters: [String:String]?, headers: [String:String]?, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
-      guard var components = URLComponents(string: path) else {
-         print("Error: could not create URL components")
-         return
-      }
-
-      // GET: parameters = Query string parameters
-      if method == "GET", let parameters = parameters {
-         components.queryItems = parameters.map { (key, value) in
-            URLQueryItem(name: key, value: value)
-         }
-         components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
-      }
-
-      var request = URLRequest(url: components.url!)
-
-      // POST or PUT: parameters = Request body parameters
-      if method == "POST" || method == "PUT" {
-         if let parameters = parameters {
-            do {
-               request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-            } catch let error {
-               print(error)
+    
+    public static func request(path: String, method: String, parameters: [String:String]?, headers: [String:String]?, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        guard var components = URLComponents(string: path) else {
+            print("Error: could not create URL components")
+            return
+        }
+        
+        // GET: parameters = Query string parameters
+        if method == "GET", let parameters = parameters {
+            components.queryItems = parameters.map { (key, value) in
+                URLQueryItem(name: key, value: value)
             }
-         }
-      }
-
-      request.httpMethod = method
-
-      if let headers = headers {
-         for (key, value) in headers {
-            request.setValue(value, forHTTPHeaderField: key)
-         }
-      }
-
-      let session = URLSession.shared
-      let task = session.dataTask(with: request) {
-         (data, response, error) in
-         completion(data, response, error)
-      }
-      task.resume()
-   }
+            components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        }
+        
+        var request = URLRequest(url: components.url!)
+        
+        // POST or PUT: parameters = Request body parameters
+        if method == "POST" || method == "PUT" {
+            if let parameters = parameters {
+                do {
+                    request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+        
+        request.httpMethod = method
+        
+        if let headers = headers {
+            for (key, value) in headers {
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+//        Log.networkInfo(request)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {
+            (data, response, error) in
+            completion(data, response, error)
+        }
+        task.resume()
+    }
 }
